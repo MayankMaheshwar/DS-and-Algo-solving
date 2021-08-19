5f7b133c0d58fc80a568c8df778c3a4c1c25e628
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-08-19 23:50:07 +0530

    added another sol for decode strings

--- leetcode/decodestring.py
+++ leetcode/decodestring.py
@@ -10,3 +10,29 @@ class Solution:
             wo_last = x + y
             wo_last_two = x
         return wo_last
+
+
+" Another Solution "
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+        dp=[0]*n
+      
+        dp[0]=0 if int(s[0])==0 else 1
+        if dp[0]==0:
+            return 0
+        
+     
+        for i in range(1,n):
+            if int(s[i])>=1:
+                dp[i]=dp[i-1]
+                
+            if int(s[i-1:i+1])>=10 and int(s[i-1:i+1])<=26:
+                if i-2==-1:
+                    dp[i]+=1
+                else:
+                    dp[i]+=dp[i-2]
+        print(dp)
+        return dp[n-1]
+                

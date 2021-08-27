21cfeae9636a645a3f41eae0b80e0dd3942ea3cb
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-08-27 23:49:53 +0530

    sliding window easy2

---
+++ leetcode/unique.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        if len(s) < 3:
+            return 0
+        return sum(s[i] != s[i-1] != s[i-2] != s[i] for i in range(2, len(s)))

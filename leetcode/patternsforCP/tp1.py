ea756c00a3e517ba055b16e1113950eff4fd7169
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-08-31 19:21:05 +0530

    added two pointer 1

---
+++ leetcode/patternsforCP/tp1.py
@@ -0,0 +1,16 @@
+" 942. DI String Match "
+
+
+class Solution:
+    def diStringMatch(self, S: str) -> List[int]:
+        lo, hi = 0, len(S)
+        ans = []
+        for x in S:
+            if x == 'I':
+                ans.append(lo)
+                lo += 1
+            else:
+                ans.append(hi)
+                hi -= 1
+
+        return ans + [lo]

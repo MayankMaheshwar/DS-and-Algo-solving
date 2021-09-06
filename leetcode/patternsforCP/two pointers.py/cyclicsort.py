281544d49ff24ce016aba766479c4e0ef4ffc3cb
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-09-06 22:10:12 +0530

    added interval pattern

--- leetcode/patternsforCP/two pointers.py/cyclicsort.py
+++ leetcode/patternsforCP/two pointers.py/cyclicsort.py
@@ -1,16 +1,17 @@
 # Cyclic Sort - Time O(n) and space O(1)
- # Sort the array with Cyclic Sort
+# Sort the array with Cyclic Sort
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         start, output = 0, []
         while start < len(nums):
-                index = nums[start]-1
+            index = nums[start]-1
 
-                if nums[index] != nums[start]:
-                    nums[index], nums[start] = nums[start], nums[index]
-                else:
-                    start += 1
-            for i, v in enumerate(nums):
-                if i+1 != v:
-                    output.append(i+1)
-            return output
+            if nums[index] != nums[start]:
+                nums[index], nums[start] = nums[start], nums[index]
+            else:
+                start += 1
+
+        for i, v in enumerate(nums):
+            if i+1 != v:
+            output.append(i+1)
+        return output

5aa21bc280d66ef3d7765a0acf06efda051c3083
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-09-13 23:52:52 +0530

    added dfs tree2str

--- leetcode/tree2str.py
+++ leetcode/tree2str.py
@@ -26,3 +26,24 @@ class Solution:
 
             return ans
         return helper(t, ans)
+
+
+""" Second dfs """
+
+
+class Solution(object):
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t:
+            return ""
+        res = ""
+        left = self.tree2str(t.left)
+        right = self.tree2str(t.right)
+        if left or right:
+            res += "(%s)" % left
+        if right:
+            res += "(%s)" % right
+        return str(t.val) + res

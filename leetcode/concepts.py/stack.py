deb928f1943d2c81ba1be4b7d4aebc023a2748cd
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-09-25 22:51:22 +0530

    added stack concept code

--- leetcode/concepts.py/stack.py
+++ leetcode/concepts.py/stack.py
@@ -2,3 +2,21 @@
 
 Return true if there is a 132 pattern in nums, otherwise, return false.
 """
+
+
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        import sys
+        stack = []
+        s3 = -sys.maxint
+        for n in nums[::-1]:
+            if n < s3:
+                return True
+            while stack and stack[-1] < n:
+                s3 = stack.pop()
+            stack.append(n)
+        return False

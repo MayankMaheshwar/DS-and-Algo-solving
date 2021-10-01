fc46f79b9d3ff5cf34cfdcc11243d800252fdb90
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-10-01 18:45:14 +0530

    added 3 largest numbers

---
+++ leetcode/concepts.py/lar3num.py
@@ -0,0 +1,15 @@
+arr = [2, 3, 1, 4534, 3, 4, 545, 3333333333333, 43, 463, 213]
+a = 0
+b = 0
+c = 0
+for num in arr:
+    if num > c:
+        a = b
+        b = c
+        c = num
+    elif num > b and num < c:
+        a = b
+        b = num
+    elif num > a and num < b:
+        a = num
+print(a, b, c)

e2f2b300d5023c594976ef1619b09d6afb91eaa3
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-10-03 06:19:12 +0530

    added hotel review code

--- leetcode/concepts.py/hotelreview.py
+++ leetcode/concepts.py/hotelreview.py
@@ -25,3 +25,27 @@ class Solution:
         B.sort(key=lambda x: (-x[0], x[1]))
 
         return [x[1] for x in B]
+
+
+class Solution:
+    # @param A : string
+    # @param B : list of strings
+    # @return a list of integers
+    def solve(self, A, B):
+        goodWords = set(A.split("_"))
+        # Making it into a set is important, as searching through a set is faster
+        V = []
+        for index in range(len(B)):
+            countGoodWords = 0
+            for word in B[index].split("_"):
+                if word in goodWords:
+                    countGoodWords += 1
+            V.append([index, countGoodWords])  # store the index and the count
+        # use the count to sort (descending order)
+        V.sort(key=lambda a: a[1], reverse=True)
+        return [x[0] for x in V]  # return the sorted list of indexes
+
+        ans = []
+        for key, value in sorted(dict.items(), key=lambda item: item[1], reverse=True):
+            ans.append(key)
+        return ans

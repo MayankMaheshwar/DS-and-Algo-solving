d5046a6d031459d4c07aa90f8da2162b0ef8b9d7
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-10-22 00:29:55 +0530

    added randomized set another code

--- leetcode/concepts.py/randomizedset.py
+++ leetcode/concepts.py/randomizedset.py
@@ -1,41 +1,25 @@
-class RandomisedSet:
+mport random
+
+class RandomizedSet(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.hashTable = {}
-        self.arr = []
-
-    def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val not in self.hashTable:
-            self.hashTable[val] = len(self.arr)
-            self.arr.append(val)
+        self.nums, self.pos = [], {}
+        
+    def insert(self, val):
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
             return True
-
         return False
+        
 
-    def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val in self.hashTable:
-
-            self.hashTable[self.arr[-1]] = self.hashTable[val]
-            self.arr[self.hashTable[val]] = self.arr[-1]
-
-            self.arr.pop()
-            self.hashTable.pop(val)
-
+    def remove(self, val):
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.nums[idx], self.pos[last] = last, idx
+            self.nums.pop(); self.pos.pop(val, 0)
             return True
-
         return False
-
-    def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        return random.choice(self.arr)
+            
+    def getRandom(self):
+        return self.nums[random.randint(0, len(self.nums) - 1)]

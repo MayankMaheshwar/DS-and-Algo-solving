03ae246209d7fa223e99877f36feb497e3db881b
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-10-24 21:44:35 +0530

    added completebst codes

--- completebst.py
+++ completebst.py
@@ -11,8 +11,7 @@ A, If it is equal, it means the left sub tree is a full binary tree
 B, It it is not , it means the right sub tree is a full binary tree
 
  class Solution:
-        # @param {TreeNode} root
-        # @return {integer}
+        
         def countNodes(self, root):
             if not root:
                 return 0

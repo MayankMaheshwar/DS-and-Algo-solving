f3e1d74629d83dac15ebfbf53a149a7b7d4d4652
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-10-30 01:42:05 +0530

    modify numcomplement ques code

--- numcomplement.py
+++ numcomplement.py
@@ -1,6 +1,10 @@
-class Solution:
-    def findComplement(self, num: int) -> int:
-        i = 1
-        while i <= num:
-            i = i << 1
-        return (i - 1) ^ num
+
+def findComplement(num):
+    i = 1
+    while i <= num:
+        i = i << 1
+        print(i)
+    return (i - 1) ^ num
+
+a=5
+print(findComplement(a))

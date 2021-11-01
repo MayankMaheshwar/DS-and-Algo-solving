2d7b43541994e040445147208d5a8699b7d60400
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-01 23:48:57 +0530

    started nov leetcode

---
+++ lcNov/nov1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        m = len(board)
+        n = len(board[0])
+        if m <= 2 or n <= 2:
+            return;
+    
+        def markBorder(i, j):
+            if 0 <= i < m and 0 <= j < n and board[i][j] is 'O':
+                board[i][j] = 'B'
+                markBorder(i + 1, j)
+                markBorder(i - 1, j)
+                markBorder(i, j + 1)
+                markBorder(i, j - 1)
+
+        for i in range(m):
+            markBorder(i, 0)
+            markBorder(i, n - 1)
+
+        for j in range(n):
+            markBorder(0, j)
+            markBorder(m - 1, j)
+
+        for i in range(m):
+            for j in range(n):
+                c = board[i][j]
+                if c is not 'X':
+                    board[i][j] = 'X' if c is 'O' else 'O'

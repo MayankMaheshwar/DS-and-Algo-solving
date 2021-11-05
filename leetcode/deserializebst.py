2d8c272bb33a15efedd8e887add92c0627a2cea7
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-05 23:55:32 +0530

    deserialize bst code

---
+++ leetcode/deserializebst.py
@@ -0,0 +1,11 @@
+def serialize(self, root):
+        def doit(node):
+            if node:
+                vals.append(str(node.val))
+                doit(node.left)
+                doit(node.right)
+            else:
+                vals.append('#')
+        vals = []
+        doit(root)
+        return ' '.join(vals)

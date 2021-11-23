e39c6d1f4d19dc48112271de6e0f3671a5232f67
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-23 23:23:41 +0530

    modify Kth smallest BST recursion code

--- kthsmallbst.py
+++ kthsmallbst.py
@@ -1,14 +1,14 @@
 stack = []
 
-  while True:
-       while root:
-            stack.append(root)
-            root = root.left
-        root = stack.pop()
-        k -= 1
-        if not k:
-            return root.val
-        root = root.right
+while True:
+    while root:
+        stack.append(root)
+        root = root.left
+    root = stack.pop()
+    k -= 1
+    if not k:
+        print(root.val)
+    root = root.right
 
 
 class Solution:
@@ -20,5 +20,5 @@ class Solution:
         """
         def inorder(r):
             return inorder(r.left) + [r.val] + inorder(r.right) if r else []
-    
+
         return inorder(root)[k - 1]

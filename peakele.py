fa3f1a831f8350bd5e56054190086228127dc844
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-23 23:33:43 +0530

    modify peak element code

--- peakele.py
+++ peakele.py
@@ -1,11 +1,13 @@
-n = len(nums)
-  left, right = 0, n - 1
-   while left <= right:
-        mid = (left + right) // 2
-        if (mid == 0 or nums[mid-1] < nums[mid]) and (mid == n-1 or nums[mid] > nums[mid+1]):  # Found peak
-            return mid
-        if mid == 0 or nums[mid-1] < nums[mid]:  # Find peak on the right
-            left = mid + 1
-        else:  # Find peak on the left
-            right = mid - 1
-    return -1
+class Solution(object):
+    def findPeakElement(self, nums):
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + (r-l)//2
+            if (mid-1 < 0 or nums[mid] > nums[mid-1]) and (mid+1 >= len(nums) or nums[mid] > nums[mid+1]):
+                return mid
+            # the opposite of the above "if" are these two "elif"
+            elif mid-1 >= 0 and nums[mid] < nums[mid-1]:
+                r = mid - 1
+            elif mid+1 < len(nums) or nums[mid] < nums[mid+1]:
+                l = mid + 1
+        return l

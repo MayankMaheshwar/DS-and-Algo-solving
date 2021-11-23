5d98445fc067d38ca5774f55a23c94a1ac6b115b
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-23 23:21:19 +0530

    added Kth smallest BST code

--- kthsmallbst.py
+++ kthsmallbst.py
@@ -1,26 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        if len(res) > 1:
-                return
+stack = []
 
-            if node.left:
-                findNode(node.left, res)
-
-            res[0] -= 1
-            if res[0] == 0:
-                res.append(node.val)
-                return
-            
-            if node.right:
-                findNode(node.right, res)
-                
-        res = [k]
-        findNode(root, res)
-        return res[1]
-        
+  while True:
+       while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if not k:
+            return root.val
+        root = root.right

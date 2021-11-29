1a874a67e29fcfd06cda604da7d7fde4593da55d
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-11-30 00:27:18 +0530

    added rotatell code

--- rotatell.py
+++ rotatell.py
@@ -1,19 +1,22 @@
-if not head or not head.next:
-    return head
+class Solution:
+    def rotateRight(self, head, k):
 
-   last, n = head, 1
-    while last.next:
-        last = last.next
-        n += 1
+        if not head or not head.next:
+            return head
 
-    if k % n == 0:
-        return head
+        last, n = head, 1
+        while last.next:
+            last = last.next
+            n += 1
 
-    middle = head
-    for i in range(n - k %n-1):
-        middle = middle.next
+        if k % n == 0:
+            return head
 
-    new_head = middle.next
-    last.next = head
-    middle.next = None
-    return new_head
+        middle = head
+        for i in range(n - k % n-1):
+            middle = middle.next
+
+        new_head = middle.next
+        last.next = head
+        middle.next = None
+        return new_head

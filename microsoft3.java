17486ec6880bbc5cc03cf5766c53e34ea6143278
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-10 23:28:43 +0530

    modify microsoft3

---
+++ microsoft3.java
@@ -0,0 +1,25 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+import java.util.*;
+class Solution {
+    public int solution(int[] A) {
+    Map<Integer, Integer> indexs = new HashMap<>();
+    int prev = 0;
+    int count = 0;
+    for (int i = 1; i < A.length; i++) {
+        int s = A[i - 1] + A[i];
+        if (s == prev && count > 0) {
+            count--;
+            continue;
+        }
+        prev = s;
+        count++;
+        indexs.put(s, indexs.getOrDefault(s, 0) + 1);
+    }
+
+    return indexs.values().stream().mapToInt(k -> k).max().getAsInt();
+}
+}

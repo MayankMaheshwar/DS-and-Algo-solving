03c1df3e023f8122af3348952f3e4696b6c0950a
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-14 16:44:31 +0530

    quantiphi

---
+++ quantiphi.py
@@ -0,0 +1,19 @@
+from itertools import groupby
+string = 'aaabbbbccccccaaa'
+
+dic = {}
+for chr in string:
+    if chr in dic:
+        dic[chr] += 1
+    else:
+        dic[chr] = 1
+
+
+mx = -2**32
+malpha = []
+for k, v in dic.items():
+    if v >= mx:
+        mx = v
+        malpha.append(k)
+
+print(mx, malpha)

c0dd0eef8bdcea7e060a6091be6f2dfc7c658dfa
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-16 23:54:06 +0530

    modify ll2

--- ll2.py
+++ ll2.py
@@ -1,39 +1,37 @@
 class node:
-    def __init__(self,data,next=None):
-        self.data=data
-        self.next=next
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
     def gd(self):
         return self.data
 
     def gn(self):
-        return self.next    
+        return self.next
 
-
-    def sd(self): 
-        self.data=data
+    def sd(self):
+        self.data = data
 
     def sn(self):
-        self.next=next
+        self.next = next
+
 
 # getting address same of objects interlinked
-a=node(0)
+a = node(0)
 print(a)
-b=node(1)
-b=node(1,a)
+b = node(1)
+b = node(1, a)
 print(b.gn())
-count=0
-# getting no. of elements in linked list and print each node's data
+count = 0
 while b:
-    count+=1
+    count += 1
     print(b.data)
-    b=b.next
-print("Count=",count)
+    b = b.next
+print("Count=", count)
 
 
-#Maintain a class for seperate linked list
+# Maintain a class for seperate linked list
 
 class list():
-    
-    self.head=None
 
+    self.head = None

477045c7eb2a27180a4b005fec5eb81825daa8a9
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-19 23:09:58 +0530

    added subsequence code

---
+++ leetcode/hem.py
@@ -0,0 +1,11 @@
+import collections
+
+
+class Solution:
+    def findLHS(self, A: List[int]) -> int:
+        count = collections.Counter(A)
+        ans = 0
+        for x in count:
+            if x+1 in count:
+                ans = max(ans, count[x] + count[x+1])
+        return ans

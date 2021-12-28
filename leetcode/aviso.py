89b52151c5704fae663d46c82e8a0d753413410a
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-28 23:55:55 +0530

    chess code

--- leetcode/aviso.py
+++ leetcode/aviso.py
@@ -1,16 +1,15 @@
 from collections import defaultdict
-dic=defaultdict(list)
+dic = defaultdict(list)
 
-string="""John is at a post, where they make pots. Making the pots will
-stop in the future. Oh dear, what will happen to them when it
-happens. This can happen only in Rome. Read on to find more."""
+string = """John is at a post, where they make pots. Making the pots will
+stop in the future. Oh dear, what d on to find more."""
 
 for str in string:
-    st=sorted(str)
+    st = sorted(str)
     if st in dic:
         dic[st].append(str)
     else:
-        dic[st]=[str]
+        dic[st] = [str]
 
 
 for key, value in dic.items():

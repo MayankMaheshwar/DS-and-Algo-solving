1042565f31f656e4e5d56a0c2879f7f9667a3e68
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-30 12:33:44 +0530

    api code

--- apicodes.py/basic.py
+++ apicodes.py/basic.py
@@ -4,23 +4,18 @@ import json
 
 class Solution:
 
-	def run(self, character):
-		#
-		# Write your code below; return type and arguments should be according to the problem's requirements
-		#
-		character = character.replace(" ", "+")
-		with urllib.request.urlopen('https://challenges.hackajob.co/swapi/api/people/?search='+character) as response:
-			html = response.read()
+    def run(self, character):
+        #
+        # Write your code below; return type and arguments should be according to the problem's requirements
+        #
+        character = character.replace(" ", "+")
+        with urllib.request.urlopen('https://challenges.hackajob.co/swapi/api/people/?search='+character) as response:
+            html = response.read()
 
 
 """ json.dumps() convert python object to a json string """
 second = "".join(chr(x) for x in bytearray(
     html))  # convert byte literal api data to string
 sd = json.loads(second)  # convert valid JSON string to python dictionary
-			numberOfFilms = (len(sd["results"][0]['films']))
-
-
-
-
-
-		return numberOfFilms
+numberOfFilms = (len(sd["results"][0]['films']))
+return numberOfFilms

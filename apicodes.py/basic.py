f0479808eb30ea2195abfb18cc8c460280cb15aa
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2021-12-30 12:31:48 +0530

    chess code

--- apicodes.py/basic.py
+++ apicodes.py/basic.py
@@ -12,14 +12,15 @@ class Solution:
 		with urllib.request.urlopen('https://challenges.hackajob.co/swapi/api/people/?search='+character) as response:
 			html = response.read()
 
-            " json.dumps() convert python object to a json string "
-			second="".join( chr(x) for x in bytearray(html) ) "convert byte literal api data to string"
-			
-            sd=json.loads(second) "convert valid JSON string to python dictionary"
-			numberOfFilms=(len(sd["results"][0]['films']))
+
+""" json.dumps() convert python object to a json string """
+			second = "".join(chr(x) for x in bytearray(html)) """convert byte literal api data to string"""
+
+			sd = json.loads(second) """convert valid JSON string to python dictionary"""
+			numberOfFilms = (len(sd["results"][0]['films']))
 
 
-			
-			
-		
+
+
+
 		return numberOfFilms

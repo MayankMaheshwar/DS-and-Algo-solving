ef40cfa54c1d6c8216d83907a74c0901e16b51ea
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-04 18:51:36 +0530

    online sales

---
+++ leetcode/online.py
@@ -0,0 +1,20 @@
+s = "4a"
+if s == "":
+    print("")
+
+previousCount = 1
+previousElement = s[0]
+ans = ""
+
+for i in range(1, len(s)):
+    cur = s[i]
+    if cur == previousElement:
+        previousCount += 1
+    else:
+        ans += previousElement+str(previousCount)
+        previousCount = 1
+        previousElement = cur
+
+
+ans += previousElement+str(previousCount)
+print(ans)

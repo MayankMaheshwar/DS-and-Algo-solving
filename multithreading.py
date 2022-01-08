5ab60db3734cbebe4fd6e2d02e84de35354a6f4d
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-08 21:05:57 +0530

    thread

--- multithreading.py
+++ multithreading.py
@@ -2,8 +2,17 @@ import _thread
 import time
 
 
-def cir(Threadname):
+def cir(A):
     cd = 10
     while cd > 0:
-        print("count")
+        print(cd, A)
         cd -= 1
+        time.sleep(2)
+
+
+try:
+    _thread.start_new_thread(cir, ("thread1",))
+except:
+    print("NA")
+while 1:
+    pass

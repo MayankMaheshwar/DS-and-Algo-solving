41bc2dc6b26b3781da8ee1a5a0740a2b2f8d681e
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-25 21:54:49 +0530

    added mountain code

--- peakele.py
+++ peakele.py
@@ -5,7 +5,6 @@ class Solution(object):
             mid = l + (r-l)//2
             if (mid-1 < 0 or nums[mid] > nums[mid-1]) and (mid+1 >= len(nums) or nums[mid] > nums[mid+1]):
                 return mid
-            # the opposite of the above "if" are these two "elif"
             elif mid-1 >= 0 and nums[mid] < nums[mid-1]:
                 r = mid - 1
             elif mid+1 < len(nums) or nums[mid] < nums[mid+1]:

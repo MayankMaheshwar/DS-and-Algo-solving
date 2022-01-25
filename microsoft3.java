eb16dc8e5fb1972eca349711e89231e057560362
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-25 21:54:20 +0530

    added mountain code

--- microsoft3.java
+++ microsoft3.java
@@ -1,8 +1,4 @@
-// you can also use imports, for example:
-// import java.util.*;
 
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 import java.util.*;
 class Solution {
     public int solution(int[] A) {

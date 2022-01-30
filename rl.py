a0fca6f2fd03d1c7bb8015cffae19f4ad3387c28
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-30 22:30:12 +0530

    remsubseq code

--- rl.py
+++ rl.py
@@ -1,12 +1,11 @@
-s="ab*c+d*e/"
+s = "ab*c+d*e/"
 stack = []
-op=('+','-','*','/')
+op = ('+', '', '*', '/')
 for i in range(len(s)):
     if s[i] not in op:
         stack.append(s[i])
     else:
         print(stack.pop(0))
         print(s[i])
-   
-print(stack.pop())
 
+print(stack.pop())

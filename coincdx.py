8de7fb88f70308c759ffdadf2d84178b3bd651d2
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-02 18:39:06 +0530

    coincdx code

--- coincdx.py
+++ coincdx.py
@@ -1,7 +1,18 @@
-""" Given string num representing a non-negative integer num, and an integer k, 
-return the smallest possible integer after removing k digits from num. 
+""" Given string num representing a non-negative integer num, and an integer k,
+return the smallest possible integer after removing k digits from num.
 
 example: Input: num = "1432219", k = 1
 Output: "1219"
 
 """
+
+
+stack = []
+   for digit in num:
+        while k > 0 and len(stack) > 0 and stack[-1] > digit:
+            k -= 1
+            stack.pop()
+        stack.append(digit)
+    if k > 0:
+        stack = stack[:-k]
+    return "".join(stack).lstrip("0") or "0"

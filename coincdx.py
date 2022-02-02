fcf9fc2bb99dd66304a561857684602d5615cb3e
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-02 18:39:21 +0530

    coincdx code

--- coincdx.py
+++ coincdx.py
@@ -7,12 +7,14 @@ Output: "1219"
 """
 
 
-stack = []
-   for digit in num:
-        while k > 0 and len(stack) > 0 and stack[-1] > digit:
-            k -= 1
-            stack.pop()
-        stack.append(digit)
-    if k > 0:
-        stack = stack[:-k]
-    return "".join(stack).lstrip("0") or "0"
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for digit in num:
+            while k > 0 and len(stack) > 0 and stack[-1] > digit:
+                k -= 1
+                stack.pop()
+            stack.append(digit)
+        if k > 0:
+            stack = stack[:-k]
+        return "".join(stack).lstrip("0") or "0"

43e1ebf7348e5157b3556266f5f983e851381490
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-03 15:20:03 +0530

    coincdx code

--- coincdx.py
+++ coincdx.py
@@ -1,8 +1,8 @@
 """ Given string num representing a non-negative integer num, and an integer k,
 return the smallest possible integer after removing k digits from num.
 
-example: Input: num = "1432219", k = 1
-Output: "1219"
+example: Input: num = "143392", k = 3
+Output: "32"
 
 """
 
@@ -15,6 +15,7 @@ class Solution:
                 k -= 1
                 stack.pop()
             stack.append(digit)
+
         if k > 0:
             stack = stack[:-k]
         return "".join(stack).lstrip("0") or "0"

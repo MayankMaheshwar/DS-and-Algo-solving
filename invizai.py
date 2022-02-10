d3a83a7b490b0bbfa0c3191b7c50005bd0e56f63
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-10 16:02:24 +0530

    invizai code

---
+++ invizai.py
@@ -0,0 +1,14 @@
+arr = [434, 12, 34, 12, 43, 5354, 3221, 42, 44, 2, 4, 16]
+no = 8
+
+flag = False
+for num in arr:
+    if num % no == 0:
+        flag = True
+
+if flag == False:
+    sum_of_arr = sum(arr)
+    if sum_of_arr % no == 0:
+        flag = True
+
+print(flag)
---
+++ invizai2.py
@@ -0,0 +1,17 @@
+str = 'abcdghabcegfgahgzeg'
+substr = 'ageg'
+
+ans = 'ageg'
+
+dic = {}
+i = 0
+j = 0
+while ans != substr:
+    while substr[j] != str[i]:
+        i += 1
+    if str[i] in ans and ans[-1] != str[i]:
+        ans = ''
+    else:
+        ans += str[i]
+        dic[str[i]] = i
+    j += 1

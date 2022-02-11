225280933b1079ab4404a20344e67743c38f1148
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-11 23:53:28 +0530

    invizai2 code

--- happyn.py
+++ happyn.py
@@ -1,18 +1,14 @@
 def hn(n):
-    t=0
-    s=list(str(n))
+    t = 0
+    s = list(str(n))
     for i in s:
-        t=t+(int(i)**2)
-    if len(str(t))==1 and t==1:
+        t = t+(int(i)**2)
+    if len(str(t)) == 1 and t == 1:
         return True
-    elif len(str(t))==1 and t!=1:
-        return False    
+    elif len(str(t)) == 1 and t != 1:
+        return False
     else:
-        return hn(t)    
-a=int(input())
+        return hn(t)
 
 
-
-
-
-
+a = int(input())

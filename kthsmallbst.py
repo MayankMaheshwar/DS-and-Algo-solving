dc9b9040a09b8d41ed7b954244c273954af9be01
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-11 23:53:42 +0530

    invizai2 code

--- kthsmallbst.py
+++ kthsmallbst.py
@@ -13,11 +13,7 @@ while True:
 
 class Solution:
     def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
+
         def inorder(r):
             return inorder(r.left) + [r.val] + inorder(r.right) if r else []
 

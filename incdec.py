bc886357f82b60307437c4a5d159b8719aa27066
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-12 22:27:02 +0530

    incdec code

--- incdec.py
+++ incdec.py
@@ -1,16 +1,18 @@
-cnt, ans, asc = collections.Counter(s), [], True
-   # if Counter not empty.
-   while cnt:
-        # traverse keys in ascending/descending order.
-        for c in sorted(cnt) if asc else reversed(sorted(cnt)):
-            # append the key.
-            ans.append(c)
-            # decrease the count.
-            cnt[c] -= 1
-            # if the count reaches to 0.
-            if cnt[c] == 0:
-                # remove the key from the Counter.
-                del cnt[c]
-        # change the direction, same as asc ^= True.
-        asc = not asc
-    return ''.join(ans)
+class Solution:
+    def sortString(self, s: str) -> str:
+        cnt, ans, asc = collections.Counter(s), [], True
+        # if Counter not empty.
+        while cnt:
+            # traverse keys in ascending/descending order.
+            for c in sorted(cnt) if asc else reversed(sorted(cnt)):
+                # append the key.
+                ans.append(c)
+                # decrease the count.
+                cnt[c] -= 1
+                # if the count reaches to 0.
+                if cnt[c] == 0:
+                    # remove the key from the Counter.
+                    del cnt[c]
+            # change the direction, same as asc ^= True.
+            asc = not asc
+        return ''.join(ans)

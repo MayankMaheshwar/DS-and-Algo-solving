c55203f668bf25a3468b5792ef0e4be9a282b243
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-15 16:30:20 +0530

    hypersonic code

---
+++ hypersonic3.py
@@ -0,0 +1,25 @@
+a = [2, 5, 8, 10]
+b = [8, 3, 6, 11]
+c = [2, 11, 8, 20, 343, 214, 2]
+
+
+def unique(a, b, c):
+    arr = [False]*(10**5)
+
+    for num in a:
+        arr[num] = True
+
+    for num in b:
+        arr[num] = True
+
+    for num in c:
+        arr[num] = True
+
+    ans = []
+    for i, num in enumerate(arr):
+        if num == True:
+            ans.append(i)
+    return ans
+
+
+print(unique(a, b, c))

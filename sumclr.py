ea9b9d41f1b9e529e1d68f6c4ac4f46471aa370d
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-21 23:07:06 +0530

    slw1 code

--- sumclr.py
+++ sumclr.py
@@ -1,22 +1,14 @@
-w=[]
+w = []
 for i in range(int(input())):
     w.extend(input().split())
-   
+
 for i in range(int(input())):
-    s=input()
-    s1=""
-    if s[len(s)-3:]=="our":
-        s1=s[:len(s)-3]+"or"
+    s = input()
+    s1 = ""
+    if s[len(s)-3:] == "our":
+        s1 = s[:len(s)-3]+"r"
     else:
-     
-        s1=s[:len(s)-2]+"our"
-     
-    
-    print(w.count(s1)+w.count(s))    
 
-        
+        s1 = s[:len(s)-2]+"our"
 
-    
-
-
-
+    print(w.count(s1)+w.count(s))

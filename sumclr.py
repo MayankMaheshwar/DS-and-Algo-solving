9d4e3e2bfa3cccb161966ce9f491d6d6413c1c6b
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-02-21 23:07:30 +0530

    slw1 code

--- sumclr.py
+++ sumclr.py
@@ -6,7 +6,7 @@ for i in range(int(input())):
     s = input()
     s1 = ""
     if s[len(s)-3:] == "our":
-        s1 = s[:len(s)-3]+"r"
+        s1 = s[:len(s)-3]+"or"
     else:
 
         s1 = s[:len(s)-2]+"our"

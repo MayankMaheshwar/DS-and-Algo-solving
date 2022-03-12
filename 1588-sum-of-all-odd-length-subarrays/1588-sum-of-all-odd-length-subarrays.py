8cc268b67c044e762788937de75e79f64b4f1e92
Mayank Maheshwari <mayank_an@srmuniv.edu.in>
2022-03-12 13:25:31 +0530

    Time: 49 ms (87.55%), Space: 13.8 MB (74.70%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        res=0
+        n=len(arr)
+        for i in range(n):
+            end=i+1
+            start=n-i
+            total=start*end
+            odd=total//2
+            if total%2==1:
+                odd+=1
+            res+=odd*arr[i]
+        return res

5edbc5db8c5f95cfd553633730680472b524cf99
Mayank Maheshwari <mayank_an@srmuniv.edu.in>
2022-09-06 14:31:07 +0530

    Time: 64 ms (23.74%), Space: 14 MB (96.47%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,12 +1,9 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[[]]
-        for i in range(1,2**len(nums)):
-            ans=[]
-            st=bin(i)[2:].zfill(len(nums))
-            for i in range(len(st)):
-                if st[i]=='1':
-                    ans.append(nums[i])
-            res.append(ans)
-        return res
+        subsets = [[]]
+
+        for num in nums:
+            for i in range(len(subsets)):
+                subsets.append(subsets[i] + [num])
+        return subsets
                 

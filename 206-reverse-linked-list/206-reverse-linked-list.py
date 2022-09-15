1884091b296639560e448f3a3fce40621d16f9d5
Mayank Maheshwari <mayank_an@srmuniv.edu.in>
2022-09-15 14:04:09 +0530

    Time: 75 ms (15.45%), Space: 20.5 MB (8.65%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -8,9 +8,9 @@ class Solution:
         if not head or not head.next:
             return head
         new_head = self.reverseList(head.next)
-        if head.next:
-            head.next.next=head
-            head.next=None
+
+        head.next.next=head
+        head.next=None
         return new_head
         
         
